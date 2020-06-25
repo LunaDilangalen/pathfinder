41dbe685665c80f8b6ffbeeee2d1528592010577
Luna Dilangalen <dadilangalen@gmail.com>
2020-06-25 22:08:44 +0800

    initial implementation of djikstra search

--- search_algorithms/djikstra.py
+++ search_algorithms/djikstra.py
@@ -1,3 +1,24 @@
-def search(graph, start):
-    print("TO DO: implement Djisktra's search")
-    # print(graph, start)
+from models import priority_queue as pq
+
+def search(graph, start, goal):
+    frontier = pq.PriorityQueue()
+    frontier.put(start, 0)
+    cameFrom = {}
+    costSoFar = {}
+    cameFrom[start] = None
+    costSoFar[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+        # Early exit condition, optional for Djikstra
+        if current == goal:
+            break
+        for next in graph.neighbors(current):
+            newCost = costSoFar[current] + graph.cost(current, next)
+            if next not in costSoFar or newCost < costSoFar[next]:
+                costSoFar[next] = newCost
+                priority = newCost
+                frontier.put(next, priority)
+                cameFrom[next] = current
+
+    return cameFrom, costSoFar
