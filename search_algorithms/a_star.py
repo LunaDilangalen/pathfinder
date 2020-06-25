d172f9365b8b8562b26a994612c14df99896a4e6
Luna Dilangalen <dadilangalen@gmail.com>
2020-06-26 00:36:43 +0800

    implemented A* search

--- search_algorithms/a_star.py
+++ search_algorithms/a_star.py
@@ -1,3 +1,29 @@
-def search(graph, start):
-    print("TO DO: implement A* search")
-    # print(graph, start)
+from models import priority_queue as pq
+
+def heuristic(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1-x2) + abs(y1-y2)
+def search(graph, start, goal):
+    frontier = pq.PriorityQueue()
+    frontier.put(start, 0)
+    cameFrom = {}
+    costSoFar = {}
+    cameFrom[start] = None
+    costSoFar[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+        # Early exit condition, optional for Djikstra
+        if current == goal:
+            print('Found goal %s' % str(current))
+            break
+        for next in graph.neighbors(current):
+            newCost = costSoFar[current] + graph.cost(current, next)
+            if next not in costSoFar or newCost < costSoFar[next]:
+                costSoFar[next] = newCost
+                priority = newCost + heuristic(goal, next)
+                frontier.put(next, priority)
+                cameFrom[next] = current
+
+    return cameFrom, costSoFar
